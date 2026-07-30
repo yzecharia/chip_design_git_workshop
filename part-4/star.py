dcf4d2a3edbdb848c5b084dc97f6a60f40a1834c
Yuval Zecharia <zechariayuval@gmail.com>
2026-07-31 00:14:28 +0300

    changed forward to 50 and added text

--- part-4/star.py
+++ part-4/star.py
@@ -2,10 +2,11 @@ from turtle import *
 color('red', 'yellow')
 begin_fill()
 while True:
-    forward(200)
+    forward(50)
     left(160)
     if abs(pos()) < 1:
         break
 end_fill()
+write("May the force be with you!", align="right")
 exitonclick()
 
