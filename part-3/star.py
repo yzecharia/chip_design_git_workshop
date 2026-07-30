f3e58e30d30c807c94948220cd6f3a0f767a8f5e
Yuval Zecharia <zechariayuval@gmail.com>
2026-07-30 23:55:28 +0300

    added multiple start

--- part-3/star.py
+++ part-3/star.py
@@ -1,11 +1,24 @@
+import math
 from turtle import *
-color('green', 'blue')
-begin_fill()
-while True:
-    forward(200)
-    left(170)
-    if abs(pos()) < 1:
-        break
-end_fill()
-exitonclick()
+def star(cx, cy, side=200, turn=170):
+    dy = (side / 2) / math.tan(math.radians(turn / 2))
+    penup()
+    goto(cx - side / 2, cy - dy)
+    setheading(0)
+    pendown()
 
+    start = pos()
+    begin_fill()
+    while True:
+        forward(side)
+        left(turn)
+        if distance(start) < 1:
+            break
+    end_fill()
+
+color('green', 'blue')
+star(-220, 0)
+star(0, 0)
+star(220, 0)
+
+exitonclick()
